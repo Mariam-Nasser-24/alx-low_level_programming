3ad72e948d0429d5c08303d4f6611724e26c5d01
Mariam Nasser <mariam@nas.com>
2023-08-10 06:40:56 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(){
-	puts("\"Programming is like building a multilingual puzzle\n"); // it takes a single argument 'string'
-	return 0;
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
