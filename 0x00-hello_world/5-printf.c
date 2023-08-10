202709a1736525c6bc58b590264b1a705433d09c
Mariam Nasser <mariam@nas.com>
2023-08-10 07:04:41 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that print using printf
+ * Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,%d");
+return (0);
+}
