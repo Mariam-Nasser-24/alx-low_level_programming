c7293a0b54940f822ee397b7637daed06be0d51f
Mariam Nasser <mariam@nas.com>
2023-08-11 13:20:09 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that print text without using printf or puts
+ * Return: 1
+ */
+int main(void)
+{
+	char sentence[90] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+	return (1);
+}
