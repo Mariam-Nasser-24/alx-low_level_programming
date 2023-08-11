edaa36888965b20a2086920217f783ba92453b5c
Mariam Nasser <mariam@nas.com>
2023-08-11 15:31:19 -0700

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A programthat prints all single digit numbers of base 10
+ * without using char
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+		putchar('\n');
+	return (0);
+}
