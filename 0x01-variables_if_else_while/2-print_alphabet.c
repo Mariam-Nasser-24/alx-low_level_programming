830a9c4ed2cd9acf6c469a4f0f1c67772011d11b
Mariam Nasser <mariam@nas.com>
2023-08-11 14:41:29 -0700

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a = 97;
+	int i = 97;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(a++);
+		putchar('\n');
+	}
+	return (0);
+}
