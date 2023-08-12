a170063ce79c90fd1b2cd3abb6a3d8d8d818645c
Mariam Nasser <mariam@nas.com>
2023-08-12 04:48:19 -0700

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all possible combinations of
+ * single-digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char o = '0';
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(o++);
+		if (i == 9)
+		{
+			putchar('$');
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+		putchar('\n');
+return (0);
+}
