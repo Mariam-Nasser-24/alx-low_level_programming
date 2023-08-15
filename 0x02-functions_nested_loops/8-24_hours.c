d7b9fb5d659e1ac6cdce61cf624dc925e2fcdb2f
Mariam Nasser <mariam@nas.com>
2023-08-15 13:44:21 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,24 +8,24 @@ void jack_bauer(void)
 {
 	int a = 0, b = 0, c = 0, d = 0;
 
-		for (a = 48; a <= 50; a++)
-		{
-			for (b = 48; b <= 57; b++)
-			{
-				for (c = 48; c <= 53; c++)
-				{
-					for (d = 48; d <= 57; d++)
-					{
-						_putchar(a);
-						_putchar(b);
-						_putchar(':');
-						_putchar(c);
-						_putchar(d);
-						_putchar('\n');
-						if (a == 50 && b == 51 && c == 53 && d == 57)
-							b = 57;
-					}
-				}
-			}
-		}
+for (a = 48; a <= 50; a++)
+{
+	for (b = 48; b <= 57; b++)
+	{
+	for (c = 48; c <= 53; c++)
+	{
+	for (d = 48; d <= 57; d++)
+	{
+		_putchar(a);
+		_putchar(b);
+		_putchar(':');
+		_putchar(c);
+		_putchar(d);
+		_putchar('\n');
+		if (a == 50 && b == 51 && c == 53 && d == 57)
+			b = 57;
+	}
+	}
+	}
+	}
 }
