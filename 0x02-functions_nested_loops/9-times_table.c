5620ace48cf05b0761b459c5fec1a9ff39c4c662
Mariam Nasser <mariam@nas.com>
2023-08-16 08:30:33 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,18 +9,18 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar('0');
+		_putchar(48);
 		for (j = 1; j < 10; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
 			r = i * j;
 			if (r > 9)
-				_putchar((r / 10) + '0');
+				_putchar((r / 10) + 48);
 			else
 				_putchar(' ');
 
-				_putchar((r % 10) + '0');
+				_putchar((r % 10) + 48);
 		}
 		_putchar('\n');
 	}
