e0c6c1b03e95e70190e52afcbe02c1b3b956926b
Mariam Nasser <mariam@nas.com>
2023-08-17 09:56:01 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - A function that prints the numbers,
+ * from 0 to 9, followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int num = 48;
+
+	while (num >= 48 && num <= 57)
+	{
+		if (num == 50 || num == 52)
+		n++;
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
+
