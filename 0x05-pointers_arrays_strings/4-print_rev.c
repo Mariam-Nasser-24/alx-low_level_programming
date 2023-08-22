3bd5ddec2148fa7c70b6a7f362ddea977f6f2001
Mariam Nasser <mariam@nas.com>
2023-08-22 03:55:26 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - A function that prints a string, in reverse
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	int i = 0, n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		n++;
+	i = 0;
+	for (i = n; i <= 0; i--)
+	_putchar(s[i]);
+	_putchar('\n');
+}
