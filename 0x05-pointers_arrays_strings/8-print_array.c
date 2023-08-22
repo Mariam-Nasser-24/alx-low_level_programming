beadeca76d9e068ef3913244098083fe058be944
Mariam Nasser <mariam@nas.com>
2023-08-22 08:30:36 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - A function that prints n elements of an array of integers
+ * @a: the array
+ * @n: the number of elements of the array
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+}
