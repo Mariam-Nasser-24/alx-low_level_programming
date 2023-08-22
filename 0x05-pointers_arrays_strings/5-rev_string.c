71a010e7fda10f2025c9d1da8edec62d8ce550c5
Mariam Nasser <mariam@nas.com>
2023-08-22 07:55:19 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,12 +5,15 @@
  */
 void rev_string(char *s)
 {
-	int i = 0;
+	int i = 0, l=0;
+	char temp;
 
-	while (s[i])
-		i++;
-	while (i--)
-		_putchar(s[i]);
-
-	_putchar('\n');
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
 }
