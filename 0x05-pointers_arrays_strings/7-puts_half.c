57555537dd7c835c1c19858cc4bf672b9c22e427
Mariam Nasser <mariam@nas.com>
2023-08-22 08:18:14 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - A function that prints half of a string
+ * @str: the pointer
+ */
+void puts_half(char *str)
+{
+	int i = 0, j = 0;
+
+	while (str[i])
+		i++;
+	if (i % 2 == 1)
+	{
+		for (j = (i - 1) / 2; j < i; j++)
+		_putchar(str[j]);
+	}
+	else
+	{
+		for (j = i / 2; j < i; j++)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
