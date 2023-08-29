b63f7541540de5d3601f6947f98f295103ffc530
Mariam Nasser <mariam@nas.com>
2023-08-29 02:40:37 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - A function that fills memory with a constant byte
+ * @s: the pointer
+ * @b: the constant
+ * @n: the max bytes
+ * Return: the pointer s
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
